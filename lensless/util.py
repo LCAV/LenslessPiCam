9a209e0fe502b956bb369432a644741141d82e87
Eric Bezzam <ebezzam@gmail.com>
2022-08-25 17:52:34 +0200

    Fix default coefficients for grayscale conversion.

--- lensless/util.py
+++ lensless/util.py
@@ -61,7 +61,7 @@ def rgb2gray(rgb, weights=None):
 
     """
     if weights is None:
-        weights = np.array([0.299, 0.587, 0.144])
+        weights = np.array([0.299, 0.587, 0.114])
     assert len(weights) == 3
     return np.tensordot(rgb, weights, axes=((2,), 0))
 
