52fd67d3b8cbe2f6d71813fd9b53f41985c22519
Eric Bezzam <ebezzam@gmail.com>
2023-06-10 02:13:57 +0200

    Add hydra support for on device capture.

--- scripts/on_device_capture.py
+++ scripts/on_device_capture.py
@@ -2,7 +2,7 @@
 Capture raw Bayer data or post-processed RGB data.
 
 ```
-python scripts/on_device_capture.py --legacy --exp 0.02 --rgb --sensor_mode 1
+python scripts/on_device_capture.py --legacy --exp 0.02 --sensor_mode 0
 ```
 
 See these code snippets for setting camera settings and post-processing
@@ -12,6 +12,7 @@ See these code snippets for setting camera settings and post-processing
 
 """
 
+import hydra
 import os
 import cv2
 import click
@@ -36,58 +37,23 @@ SENSOR_MODES = [
 ]
 
 
-@click.command()
-@click.option(
-    "--fn",
-    default="test",
-    type=str,
-    help="File name for recorded image.",
-)
-@click.option(
-    "--exp",
-    default=0.5,
-    type=float,
-    help="Exposure time in seconds.",
-)
-@click.option(
-    "--config_pause",
-    default=2,
-    type=float,
-    help="Pause time for loading / setting camera configuration.",
-)
-@click.option(
-    "--sensor_mode",
-    default="0",
-    type=click.Choice(np.arange(len(SENSOR_MODES)).astype(str)),
-    help="{'off': 0, 'auto': 1, 'sunlight': 2, 'cloudy': 3, 'shade': 4, 'tungsten': 5, "
-    "'fluorescent': 6, 'incandescent': 7, 'flash': 8, 'horizon': 9}",
-)
-@click.option(
-    "--rgb", is_flag=True, help="Whether to reconstruct RGB data or return raw bayer data."
-)
-@click.option(
-    "--gray",
-    is_flag=True,
-    help="Get grayscale data from the Raspberry Pi.",
-)
-@click.option("--iso", default=100, type=int)
-@click.option(
-    "--sixteen",
-    is_flag=True,
-)
-@click.option(
-    "--nbits_out",
-    default=8,
-    type=int,
-    help="Number of bits for output. Only used if saving RGB data.",
-)
-@click.option(
-    "--legacy",
-    is_flag=True,
-    help="Whether to use legacy camera software, despite being on Bullseye.",
-)
-@click.option("--down", type=float, help="Factor by which to downsample output.", default=None)
-def capture(fn, exp, config_pause, sensor_mode, iso, sixteen, rgb, nbits_out, legacy, gray, down):
+@hydra.main(version_base=None, config_path="../configs", config_name="capture")
+def capture(config):
+
+    bayer = config.bayer
+    fn = config.fn
+    exp = config.exp
+    config_pause = config.config_pause
+    sensor_mode = config.sensor_mode
+    rgb = config.rgb
+    gray = config.gray
+    iso = config.iso
+    sixteen = config.sixteen
+    legacy = config.legacy
+    down = config.down
+    res = config.res
+    nbits_out = config.nbits_out
+
     # https://www.raspberrypi.com/documentation/accessories/camera.html#maximum-exposure-times
     # TODO : check which camera
     assert exp <= 230
@@ -123,8 +89,8 @@ def capture(fn, exp, config_pause, sensor_mode, iso, sixteen, rgb, nbits_out, le
             stdout=subprocess.PIPE,
             stderr=subprocess.PIPE,
         )
-        result = cmd.stdout.readlines()
-        error = cmd.stderr.readlines()
+        cmd.stdout.readlines()
+        cmd.stderr.readlines()
         os.remove(jpg_fn)
         os.system(f"exiftool {dng_fn}")
         print("\nJPG saved to : {}".format(jpg_fn))
@@ -132,73 +98,175 @@ def capture(fn, exp, config_pause, sensor_mode, iso, sixteen, rgb, nbits_out, le
     else:
         import picamerax.array
 
-        camera = picamerax.PiCamera(framerate=1 / exp, sensor_mode=sensor_mode)
-
-        # camera settings, as little processing as possible
-        camera.iso = iso
-        camera.shutter_speed = int(exp * 1e6)
-        camera.exposure_mode = "off"
-        camera.drc_strength = "off"
-        camera.image_denoise = False
-        camera.image_effect = "none"
-        camera.still_stats = False
-
-        sleep(config_pause)
-        awb_gains = camera.awb_gains
-        camera.awb_mode = "off"
-        camera.awb_gains = awb_gains
-
-        print("Resolution : {}".format(camera.MAX_RESOLUTION))
-        print("Shutter speed : {}".format(camera.shutter_speed))
-        print("ISO : {}".format(camera.iso))
-        print("Frame rate : {}".format(camera.framerate))
-        print("Sensor mode : {}".format(SENSOR_MODES[sensor_mode]))
-        print("Config load time : {} seconds".format(config_pause))
-        # keep this as it needs to be parsed from remote script!
-        red_gain = float(awb_gains[0])
-        blue_gain = float(awb_gains[1])
-        print("Red gain : {}".format(red_gain))
-        print("Blue gain : {}".format(blue_gain))
-
-        # capture data
-        stream = picamerax.array.PiBayerArray(camera)
-        camera.capture(stream, "jpeg", bayer=True)
         fn += ".png"
 
-        # get bayer data
-        if sixteen:
-            output = np.sum(stream.array, axis=2).astype(np.uint16)
-        else:
-            output = (np.sum(stream.array, axis=2) >> 2).astype(np.uint8)
+        if bayer:
 
-        if rgb or gray:
+            # if rgb:
+
+            #     camera = picamerax.PiCamera(framerate=1 / exp, sensor_mode=sensor_mode, resolution=res)
+            #     camera.iso = iso
+            #     # Wait for the automatic gain control to settle
+            #     sleep(config_pause)
+            #     # Now fix the values
+            #     camera.shutter_speed = camera.exposure_speed
+            #     camera.exposure_mode = "off"
+            #     g = camera.awb_gains
+            #     camera.awb_mode = "off"
+            #     camera.awb_gains = g
+
+            #     print("Resolution : {}".format(camera.resolution))
+            #     print("Shutter speed : {}".format(camera.shutter_speed))
+            #     print("ISO : {}".format(camera.iso))
+            #     print("Frame rate : {}".format(camera.framerate))
+            #     print("Sensor mode : {}".format(SENSOR_MODES[sensor_mode]))
+            #     # keep this as it needs to be parsed from remote script!
+            #     red_gain = float(g[0])
+            #     blue_gain = float(g[1])
+            #     print("Red gain : {}".format(red_gain))
+            #     print("Blue gain : {}".format(blue_gain))
+
+            #     # take picture
+            #     fn += ".png"
+            #     camera.capture(str(fn), bayer=False, resize=None)
+
+            # else:
+
+            camera = picamerax.PiCamera(framerate=1 / exp, sensor_mode=sensor_mode, resolution=res)
+
+            # camera settings, as little processing as possible
+            camera.iso = iso
+            camera.shutter_speed = int(exp * 1e6)
+            camera.exposure_mode = "off"
+            camera.drc_strength = "off"
+            camera.image_denoise = False
+            camera.image_effect = "none"
+            camera.still_stats = False
+
+            sleep(config_pause)
+            awb_gains = camera.awb_gains
+            camera.awb_mode = "off"
+            camera.awb_gains = awb_gains
+
+            print("Resolution : {}".format(camera.resolution))
+            print("Shutter speed : {}".format(camera.shutter_speed))
+            print("ISO : {}".format(camera.iso))
+            print("Frame rate : {}".format(camera.framerate))
+            print("Sensor mode : {}".format(SENSOR_MODES[sensor_mode]))
+            # keep this as it needs to be parsed from remote script!
+            red_gain = float(awb_gains[0])
+            blue_gain = float(awb_gains[1])
+            print("Red gain : {}".format(red_gain))
+            print("Blue gain : {}".format(blue_gain))
+
+            # capture data
+            stream = picamerax.array.PiBayerArray(camera)
+            camera.capture(stream, "jpeg", bayer=True)
+            fn += ".png"
+
+            # get bayer data
             if sixteen:
-                n_bits = 12  # assuming Raspberry Pi HQ
+                output = np.sum(stream.array, axis=2).astype(np.uint16)
             else:
-                n_bits = 8
-            output_rgb = bayer2rgb(
-                output,
-                nbits=n_bits,
-                blue_gain=blue_gain,
-                red_gain=red_gain,
-                black_level=RPI_HQ_CAMERA_BLACK_LEVEL,
-                ccm=RPI_HQ_CAMERA_CCM_MATRIX,
-                nbits_out=nbits_out,
-            )
+                output = (np.sum(stream.array, axis=2) >> 2).astype(np.uint8)
 
-            if down:
-                output_rgb = resize(output_rgb, 1 / down, interpolation=cv2.INTER_CUBIC)
+            if rgb or gray:
+                if sixteen:
+                    n_bits = 12  # assuming Raspberry Pi HQ
+                else:
+                    n_bits = 8
+                output_rgb = bayer2rgb(
+                    output,
+                    nbits=n_bits,
+                    blue_gain=blue_gain,
+                    red_gain=red_gain,
+                    black_level=RPI_HQ_CAMERA_BLACK_LEVEL,
+                    ccm=RPI_HQ_CAMERA_CCM_MATRIX,
+                    nbits_out=nbits_out,
+                )
 
-            # need OpenCV to save 16-bit RGB image
-            if gray:
-                output_gray = rgb2gray(output_rgb)
-                output_gray = output_gray.astype(output.dtype)
-                cv2.imwrite(fn, output_gray)
+                if down:
+                    output_rgb = resize(output_rgb, 1 / down, interpolation=cv2.INTER_CUBIC)
+
+                # need OpenCV to save 16-bit RGB image
+                if gray:
+                    output_gray = rgb2gray(output_rgb)
+                    output_gray = output_gray.astype(output.dtype)
+                    cv2.imwrite(fn, output_gray)
+                else:
+                    cv2.imwrite(fn, cv2.cvtColor(output_rgb, cv2.COLOR_RGB2BGR))
             else:
-                cv2.imwrite(fn, cv2.cvtColor(output_rgb, cv2.COLOR_RGB2BGR))
+                img = Image.fromarray(output)
+                img.save(fn)
+
         else:
-            img = Image.fromarray(output)
-            img.save(fn)
+
+            from picamerax import PiCamera
+
+            camera = PiCamera()
+            if res:
+                assert len(res) == 2
+                assert np.prod(res) < np.prod([2600, 2100]), "Use bayer for higher resolution."
+                camera.resolution = tuple(res)
+
+            camera.capture(fn)
+
+            # else:
+            #     res = camera.MAX_RESOLUTION
+
+            # if np.prod(res) > np.prod([2600, 2100]):
+
+            #     # use raspistill
+            #     import subprocess
+
+            #     pic_command = [
+            #         "raspistill",
+            #         "-o",
+            #         "test.png",
+            #         "-w",
+            #         f"{res[0]}",
+            #         "-h",
+            #         f"{res[1]}",
+            #     ]
+
+            #     print("using raspistill")
+
+            #     os.system(f"raspistill -o test.png -w {res[0]} -h {res[1]}")
+            # cmd = subprocess.Popen(
+            #     pic_command,
+            #     shell=False,
+            #     stdout=subprocess.PIPE,
+            #     stderr=subprocess.PIPE,
+            # )
+            # result = cmd.stdout.readlines()
+            # error = cmd.stderr.readlines()
+
+            # if error != []:
+            #     print("ERROR: %s" % error)
+            #     return
+            # if result == []:
+            #     error = ssh.stderr.readlines()
+            #     print("ERROR: %s" % error)
+            #     return
+            # else:
+            #     result = [res.decode("UTF-8") for res in result]
+            #     result = [res for res in result if len(res) > 3]
+            #     result_dict = dict()
+            #     for res in result:
+            #         _key = res.split(":")[0].strip()
+            #         _val = "".join(res.split(":")[1:]).strip()
+            #         result_dict[_key] = _val
+            #     # result_dict = dict(map(lambda s: map(str.strip, s.split(":")), result))
+            #     print("COMMAND OUTPUT : ")
+            #     pprint(result_dict)
+
+            # else:
+
+            #     camera.resolution = tuple(res)
+            #     # camera.start_preview()
+            #     # Camera warm-up time
+            #     # sleep(config_pause)
+            #     camera.capture(fn)
 
         print("\nImage saved to : {}".format(fn))
 
