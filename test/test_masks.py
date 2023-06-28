65a4d196b8f12741777eb3c10ad9d3c499e0d9e1
Aaron Fargeon <aaron.fargeon@eleves.enpc.fr>
2023-06-28 14:06:41 +0200

    testing from_sensor class method

--- test/test_masks.py
+++ test/test_masks.py
@@ -15,32 +15,32 @@ def test_flatcam():
 
     mask1 = CodedAperture(method='MURA', 
                           n_bits=25, 
-                          sensor_size_px=(4096,3040), 
+                          sensor_size_px=(380,507,3), 
                           sensor_size_m=None, 
                           feature_size=d1, 
                           distance_sensor=dz, 
                           wavelength=lambd)
-    assert mask1.mask.shape == (4096, 3040)
+    assert mask1.mask.shape == (380,507,3)
 
     mask2 = CodedAperture(method='MLS', 
                           n_bits=5, 
-                          sensor_size_px=(4096,3040), 
+                          sensor_size_px=(380,507,3), 
                           sensor_size_m=None, 
                           feature_size=d1, 
                           distance_sensor=dz, 
                           wavelength=lambd)
-    assert mask2.mask.shape == (4096, 3040)
+    assert mask2.mask.shape == (380,507,3)
 
 
 def test_phlatcam():
     
     mask = PhaseContour(noise_period=(8,8),  
-                         sensor_size_px=(256,256), 
+                         sensor_size_px=(380,507), 
                          sensor_size_m=None, 
                          feature_size=d1, 
                          distance_sensor=dz, 
                          wavelength=lambd)
-    assert mask.mask.shape == (256, 256)
+    assert mask.mask.shape == (380,507,3)
 
     Mp = np.sqrt(mask.target_psf) * np.exp(1j * np.angle(fresnel_conv(mask.mask, lambd, d1, dz, dtype=np.float32)[0]))
     assert mse(abs(Mp), np.sqrt(mask.target_psf)) < 0.1
@@ -51,14 +51,21 @@ def test_phlatcam():
 def test_fza():
 
     mask = FresnelZoneAperture(radius=30.,  
-                                sensor_size_px=(512,512), 
+                                sensor_size_px=(380,507), 
                                 sensor_size_m=None, 
                                 feature_size=d1, 
                                 distance_sensor=dz, 
                                 wavelength=lambd)
-    assert mask.mask.shape == (512, 512)
+    assert mask.mask.shape == (380,507,3)
 
 
-test_flatcam()
-test_phlatcam()
-test_fza()
+def test_classmethod():
+
+    mask = CodedAperture.from_sensor(distance_sensor=4e-3)
+    assert mask.psf.shape == (380,507,3)
+
+
+#test_flatcam()
+#test_phlatcam()
+#test_fza()
+test_classmethod()
