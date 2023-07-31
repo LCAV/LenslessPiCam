572e71ae00430ac2552b318e14387b86b33f0492
Aaron Fargeon <aaron.fargeon@eleves.enpc.fr>
2023-07-31 17:00:40 +0200

    implemented tikhonov reconstruction

--- scripts/sim/mask_dataset.py
+++ scripts/sim/mask_dataset.py
@@ -1,7 +1,47 @@
+"""
+
+Simulate a mask, simulate a few measurements with it, and reconstruct the images.
+
+Procedure is as follows:
+
+1) Simulate the mask.
+2) Simulate measurements with the mask and specified physical parameters.
+3) Reconstruct the images from the measurements.
+
+Example usage:
+
+Simulate FlatCam with separable simulation and Tikhonov reconstuction (https://arxiv.org/abs/1509.00116, Eq 7):
+```
+python scripts/sim/mask_dataset.py mask.type=MLS simulation.flatcam=True recon.algo=tikhonov
+```
+
+Simulate FlatCam with PSF simulation and Tikhonov reconstuction:
+```
+python scripts/sim/mask_dataset.py mask.type=MLS simulation.flatcam=False recon.algo=tikhonov
+```
+
+Simulate FlatCam with PSF simulation and ADMM reconstruction:
+```
+python scripts/sim/mask_dataset.py mask.type=MLS simulation.flatcam=False recon.algo=admm
+```
+
+Simulate Fresnel Zone Aperture camera with PSF simulation and ADMM reconstuction (https://www.nature.com/articles/s41377-020-0289-9):
+```
+python scripts/sim/mask_dataset.py mask.type=FZA recon.algo=admm
+```
+
+Simulate PhaseContour camera with PSF simulation and ADMM reconstuction (https://ieeexplore.ieee.org/document/9076617):
+```
+python scripts/sim/mask_dataset.py mask.type=PhaseContour recon.algo=admm
+```
+
+"""
+
 import hydra
+import warnings
 from hydra.utils import to_absolute_path
 from lensless.utils.io import load_image, load_psf, save_image
-from lensless.utils.image import rgb2gray, resize, rgb2bayer, bayer2rgb
+from lensless.utils.image import rgb2gray, resize
 import numpy as np
 from lensless import ADMM
 from lensless.eval.metric import mse, psnr, ssim, lpips, LPIPS_MIN_DIM
@@ -9,65 +49,20 @@ from waveprop.simulation import FarFieldSimulator
 import glob
 import os
 from tqdm import tqdm
-from numpy.linalg import multi_dot
-from scipy.linalg import circulant
-from waveprop.noise import add_shot_noise
 from lensless.hardware.mask import CodedAperture, PhaseContour, FresnelZoneAperture
 from lensless.recon.tikhonov import CodedApertureReconstruction
-import matplotlib.pyplot as plt
 
 
-def conv_matrices(img_shape, mask):
-    P = circulant(np.resize(mask.col, mask.mask.shape[0]))[:, : img_shape[0]]
-    Q = circulant(np.resize(mask.row, mask.mask.shape[1]))[:, : img_shape[1]]
-    return P, Q
-
-
-def fc_simulation(img, mask, P=None, Q=None, format="RGB", SNR=40):
-    """
-    Simulation function
-    """
-    format = format.lower()
-    assert format in [
-        "grayscale",
-        "rgb",
-        "bayer_rggb",
-        "bayer_bggr",
-        "bayer_grbg",
-        "bayer_gbrg",
-    ], "color_profile must be in ['grayscale', 'rgb', 'bayer_rggb', 'bayer_bggr', 'bayer_grbg', 'bayer_gbrg']"
-    # print(f"######### SQUEEZE SHAPE:", img.squeeze().shape, "#########")
-    if len(img.squeeze().shape) == 2:
-        # print('here 1')
-        n_channels = 1
-        img_ = img.copy()
-    elif format == "grayscale":
-        # print('here 2')
-        n_channels = 1
-        img_ = rgb2gray(img)
-    elif format == "rgb":
-        n_channels = 3
-        img_ = img.copy()
-    else:
-        n_channels = 4
-        img_ = rgb2bayer(img, pattern=format[-4:])
-    if P is None:
-        P = circulant(np.resize(mask.col, mask.mask.shape[0]))[:, : img.shape[0]]
-    if Q is None:
-        Q = circulant(np.resize(mask.row, mask.mask.shape[1]))[:, : img.shape[1]]
-    Y = np.dstack([multi_dot([P, img_[:, :, c], Q.T]) for c in range(n_channels)])
-    # Y = (Y - Y.min()) / (Y.max() - Y.min())
-    # Y = add_shot_noise(Y, snr_db=SNR)
-    Y = (Y - Y.min()) / (Y.max() - Y.min())
-
-    return Y
-
-
-@hydra.main(version_base=None, config_path="../../configs", config_name="simulate_dataset_custom")
+@hydra.main(version_base=None, config_path="../../configs", config_name="mask_sim_dataset")
 def simulate(config):
 
     # set seed
     np.random.seed(config.seed)
+
+    mask2sensor = config.simulation.mask2sensor
+    sensor = config.simulation.sensor
+    snr_db = config.simulation.snr_db
+    downsample = config.simulation.downsample
 
     dataset = to_absolute_path(config.files.dataset)
     if not os.path.isdir(dataset):
@@ -87,49 +82,55 @@ def simulate(config):
                 url, os.path.dirname(dataset), filename=filename, remove_finished=True
             )
 
+    mask_type = config.mask.mask_type
+
     # psf_fp = to_absolute_path(config.files.psf)
     # assert os.path.exists(psf_fp), f"PSF {psf_fp} does not exist."
 
+    # check for flatcam simulation
+    flatcam_sim = config.simulation.flatcam
+    if flatcam_sim and mask_type.upper() not in ["MURA", "MLS"]:
+        warnings.warn(
+            "Flatcam simulation only supported for MURA and MLS masks. Using far field simulation with PSF."
+        )
+        flatcam_sim = False
+
     if config.save.bool:
-        save_dir = to_absolute_path(config.save.dir + config.simulation.mask_type)
+        if flatcam_sim:
+            save_dir = to_absolute_path(config.save.dir + mask_type + "_flatcam")
+        else:
+            save_dir = to_absolute_path(config.save.dir + mask_type)
         if not os.path.exists(save_dir):
             os.makedirs(save_dir)
             os.makedirs(os.path.join(save_dir, "sensor_plane"))
             os.makedirs(os.path.join(save_dir, "object_plane"))
             os.makedirs(os.path.join(save_dir, "reconstruction"))
 
-    ## load psf as numpy array
-    # print("\nPSF:")
-    # psf = load_psf(psf_fp, verbose=True, downsample=config.simulation.downsample)
-
-    # remove depth dimension, as 3D not supported by FarFieldSimulator
-    # psf_sim = psf[0]
-    # feature_size = np.random.uniform(config.simulation.object_height[0], config.simulation.object_height[1])
-    if config.simulation.mask_type.upper() in ["MURA", "MLS"]:
+    # simulate mask
+    if config.mask.mask_type.upper() in ["MURA", "MLS"]:
         mask = CodedAperture.from_sensor(
-            sensor_name="rpi_hq",
-            downsample=8,
-            method=config.simulation.mask_type,
-            n_bits=config.simulation.n_bits,
-            distance_sensor=config.simulation.mask2sensor,
+            sensor_name=sensor,
+            downsample=downsample,
+            method=mask_type,
+            distance_sensor=mask2sensor,
+            **config.mask,
         )
         psf_sim = mask.psf / np.linalg.norm(mask.psf.ravel())
-    elif config.simulation.mask_type.upper() == "FZA":
+    elif mask_type.upper() == "FZA":
         mask = FresnelZoneAperture.from_sensor(
-            sensor_name="rpi_hq",
-            downsample=8,
-            radius=config.simulation.radius,
-            distance_sensor=config.simulation.mask2sensor,
+            sensor_name=sensor,
+            downsample=downsample,
+            distance_sensor=mask2sensor,
+            **config.mask,
         )
         psf_sim = mask.psf / np.linalg.norm(mask.psf.ravel())
-    elif config.simulation.mask_type.lower() == "phase":
+    elif config.mask.mask_type.lower() == "phase":
         mask = PhaseContour.from_sensor(
-            sensor_name="rpi_hq",
-            downsample=8,
-            noise_period=config.simulation.noise_period,
-            refractive_index=config.simulation.refractive_index,
-            n_iter=config.simulation.phase_mask_iter,
-            distance_sensor=config.simulation.mask2sensor,
+            sensor_name=sensor,
+            downsample=downsample,
+            distance_sensor=mask2sensor,
+            n_iter=config.mask.phase_mask_iter,
+            **config.mask,
         )
         psf_sim = mask.psf / np.linalg.norm(mask.psf.ravel())
     else:
@@ -140,10 +141,7 @@ def simulate(config):
 
     if config.simulation.grayscale and len(psf_sim.shape) == 3:
         psf_sim = rgb2gray(psf_sim)
-    print("\n", psf_sim.min(), psf_sim.max(), "\n")
-    plt.figure(figsize=(10, 10)), plt.imshow(psf_sim * 255, cmap="gray"), plt.colorbar()
-    plt.title("PSF for the simulation")  # , plt.xticks([]), plt.yticks([])
-    plt.show()
+
     if config.simulation.downsample > 1:
         print(f"Downsampled to {psf_sim.shape}.")
 
@@ -155,15 +153,19 @@ def simulate(config):
     files = glob.glob(os.path.join(dataset, f"*.{config.files.image_ext}"))
     if config.files.n_files is not None:
         files = files[: config.files.n_files]
+
     for fp in tqdm(files):
 
         # load image as numpy array
-        image = load_image(fp)
+        image = load_image(fp) / 255
         if config.simulation.grayscale and len(image.shape) == 3:
             image = rgb2gray(image)
 
         # simulate image
         image_plane, object_plane = simulator.propagate(image, return_object_plane=True)
+        if flatcam_sim:
+            image_plane = mask.simulate(object_plane, snr_db=snr_db)
+
         if config.save.bool:
 
             bn = os.path.basename(fp).split(".")[0] + ".png"
@@ -177,7 +179,9 @@ def simulate(config):
             save_image(image_plane, lensless_fp, max_val=config.simulation.max_val)
 
     # reconstruction
-    if config.admm.bool:
+    recon_algo = config.recon.algo.lower()
+
+    if config.recon.bool:
 
         print("\nReconstructing lensless measurements...")
 
@@ -189,8 +193,13 @@ def simulate(config):
             psf = resize(psf, shape=config.simulation.output_dim)
 
         # -- initialize reconstruction object
-        # recon = ADMM(psf, **config.admm)
-        recon = ADMM(psf_sim[np.newaxis, :, :, :], **config.admm)
+        # recon = ADMM(psf, **config.recon)
+        if recon_algo == "tikhonov":
+            recon = CodedApertureReconstruction(
+                mask, object_plane.shape, lmbd=config.recon.tikhonov.reg
+            )
+        elif recon_algo == "admm":
+            recon = ADMM(psf_sim[np.newaxis, :, :, :], **config.recon)
 
         # -- metrics
         mse_vals = []
@@ -210,19 +219,17 @@ def simulate(config):
 
             lensless = load_image(fp, as_4d=True)
             lensless = lensless / np.max(lensless)
-            recon.set_data(lensless)
-            res, _ = recon.apply(n_iter=config.admm.n_iter, disp_iter=config.admm.disp_iter)
-            recovered = res[0]  # first depth
+            if recon_algo == "tikhonov":
+                recovered = recon.apply(lensless[0])
+            elif recon_algo == "admm":
+                recon.set_data(lensless)
+                res, _ = recon.apply(n_iter=config.recon.n_iter, disp_iter=config.recon.disp_iter)
+                recovered = res[0]  # first depth
 
             if config.save.bool:
                 bn = os.path.basename(fp).split(".")[0] + ".png"
                 lensless_fp = os.path.join(save_dir, "reconstruction", bn)
 
-                # need float image for gamma correction and plotting
-                # gamma = 2.2
-                # img_norm = img / img.max()
-                # if gamma and gamma > 1:
-                #    img_norm = gamma_correction(img_norm, gamma=gamma)
                 save_image(recovered, lensless_fp, max_val=config.simulation.max_val)
 
             # compute metrics
@@ -232,7 +239,7 @@ def simulate(config):
             if config.simulation.output_dim is not None:
                 # best would be to incorporate downsampling in the reconstruction
                 # for now downsample the PSF
-                # print("-- Resizing object plane to", config.simulation.output_dim, "for metric.")
+                print("-- Resizing object plane to", config.simulation.output_dim, "for metric.")
                 object_plane = resize(object_plane, shape=config.simulation.output_dim)
 
             mse_vals.append(mse(object_plane, recovered))
