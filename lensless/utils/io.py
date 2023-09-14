Fix normalization.

lensless/utils/io.py
@@ -34,6 +34,7 @@ def load_image(
     return_float=False,
     shape=None,
     dtype=None,
+    normalize=True,
 ):
     """
     Load image as numpy array.
@@ -73,6 +74,8 @@ def load_image(
         Shape (H, W, C) to resize to.
     dtype : str, optional
         Data type of returned data. Default is to use that of input.
+    normalize : bool, default True
+        If ``return_float``, whether to normalize data to maximum value of 1.
 
     Returns
     -------
@@ -160,7 +163,8 @@ def load_image(
             dtype = np.float32
         assert dtype == np.float32 or dtype == np.float64
         img = img.astype(dtype)
-        img /= img.max()
+        if normalize:
+            img /= img.max()
 
     else:
         if dtype is None:
@@ -336,6 +340,7 @@ def load_psf(
 def load_data(
     psf_fp,
     data_fp,
+    return_float=True,
     downsample=None,
     bg_pix=(5, 25),
     plot=True,
@@ -361,6 +366,8 @@ def load_data(
         Full path to PSF file.
     data_fp : str
         Full path to measurement file.
+    return_float : bool, optional
+        Whether to return PSF as float array, or unsigned int.
     downsample : int or float
         Downsampling factor.
     bg_pix : tuple, optional
@@ -388,7 +395,7 @@ def load_data(
         in "Learned reconstructions for practical mask-based lensless imaging"
         of Kristina Monakhova et. al.
     normalize : bool default True
-        Whether to normalize data to unit norm.
+        Whether to normalize data to maximum value of 1.
 
     Returns
     -------
@@ -418,7 +425,7 @@ def load_data(
     psf, bg = load_psf(
         psf_fp,
         downsample=downsample,
-        return_float=True,
+        return_float=return_float,
         bg_pix=bg_pix,
         return_bg=True,
         flip=flip,
@@ -440,16 +447,14 @@ def load_data(
         red_gain=red_gain,
         bg=bg,
         as_4d=True,
-        return_float=True,
+        return_float=return_float,
         shape=shape,
+        normalize=normalize,
     )
 
     if data.shape != psf.shape:
         # in DiffuserCam dataset, images are already reshaped
         data = resize(data, shape=psf.shape)
-
-    if normalize:
-        data /= np.linalg.norm(data.ravel())
 
     if data.shape[3] > 1 and psf.shape[3] == 1:
         warnings.warn(

scripts/recon/admm.py
@@ -43,8 +43,7 @@ def admm(config):
         torch=config.torch,
         torch_device=config.torch_device,
         bg_pix=config.preprocess.bg_pix,
-        # TODO normalize?
-        normalize=not config["admm"]["unrolled"],  # unrolled ADMM assumes unnormalized data
+        normalize=config.preprocess.normalize,
     )
 
     disp = config["display"]["disp"]
@@ -88,6 +87,7 @@ def admm(config):
         print("Loading checkpoint from : ", path)
         assert os.path.exists(path), "Checkpoint does not exist"
         recon.load_state_dict(torch.load(path, map_location=config.torch_device))
+
     recon.set_data(data)
     print(f"Setup time : {time.time() - start_time} s")
 
@@ -138,7 +138,7 @@ def admm(config):
             if res[2] is not None:
                 pre_post_process_image = res[2].cpu().numpy()
                 ax = plot_image(pre_post_process_image, gamma=config["display"]["gamma"])
-                ax.set_title("Image after preprocessing")
+                ax.set_title("Image prior to post-processing")
                 plt.savefig(plib.Path(save) / "pre_post_process.png")
 
         np.save(plib.Path(save) / "final_reconstruction.npy", img)