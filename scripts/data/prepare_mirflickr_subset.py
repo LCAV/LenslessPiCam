a1037a78a2222dbba90754a3313bad51cbe47051
Eric Bezzam <ebezzam@gmail.com>
2023-09-20 15:57:48 +0000

    Clean up script for DiffuserCam subset.

--- scripts/data/prepare_mirflickr_subset.py
+++ scripts/data/prepare_mirflickr_subset.py
@@ -11,7 +11,11 @@ python scripts/data/prepare_mirflickr_subset.py \
 
 """
 
-import click
+import hydra
+from hydra.utils import get_original_cwd
+from lensless.utils.dataset import MeasuredDataset, DiffuserCamMirflickr
+import tqdm
+
 import glob
 import numpy as np
 from datetime import datetime
@@ -20,34 +24,83 @@ import pathlib as plib
 from shutil import copyfile
 from PIL import Image
 from lensless.recon.mirflickr import postprocess
+from torch.utils.data import Subset
 
 
-@click.command()
-@click.option(
-    "--data",
-    type=str,
-    help="Path to original dataset.",
+# @click.command()
+# @click.option(
+#     "--data",
+#     type=str,
+#     help="Path to original dataset.",
+# )
+# @click.option(
+#     "--n_files",
+#     type=int,
+#     default=200,
+#     help="Number of files in subset.",
+# )
+# @click.option(
+#     "--seed",
+#     type=int,
+#     default=11,
+# )
+# @click.option(
+#     "--output_dir_path",
+#     type=str,
+#     help="Output directory. Otherwise save subset in running directory.",
+# )
+# def subset_mirflickr(data, n_files, seed, output_dir_path):
+@hydra.main(
+    version_base=None, config_path="../../configs", config_name="diffusercam_mirflickr_subset"
 )
-@click.option(
-    "--n_files",
-    type=int,
-    default=200,
-    help="Number of files in subset.",
-)
-@click.option(
-    "--seed",
-    type=int,
-    default=11,
-)
-@click.option(
-    "--output_dir_path",
-    type=str,
-    help="Output directory. Otherwise save subset in running directory.",
-)
-def subset_mirflickr(data, n_files, seed, output_dir_path):
-    diffuser_dir = os.path.join(os.path.join(data, "dataset"), "diffuser_images")
-    lensed_dir = os.path.join(os.path.join(data, "dataset"), "ground_truth_lensed")
-    psf_path = os.path.join(data, "psf.tiff")
+def subset_mirflickr(config):
+
+    original_path = os.path.join(get_original_cwd(), config.original_path)
+    psf_path = os.path.join(get_original_cwd(), config.psf_path)
+
+    # load dataset
+    dataset = DiffuserCamMirflickr(
+        dataset_dir=original_path,
+        psf_path=psf_path,
+    )
+
+    # take subset
+    seed = config.seed
+    n_files = config.n_files
+    if seed is not None:
+        np.random.seed(seed)
+        indices = np.random.choice(dataset.allowed_idx, n_files, replace=False)
+    else:
+        indices = dataset.allowed_idx[dataset.allowed_idx <= n_files]
+
+    subset = Subset(dataset, indices)
+    for i in tqdm.tqdm(range(len(subset))):
+        lensless, lensed = subset[i]
+
+    # h = dataset[0]
+
+    dataset = MeasuredDataset(
+        root_dir=original_path,
+        psf=psf_path,
+        lensless_fn="diffuser_images",
+        lensed_fn="ground_truth_lensed",
+        downsample=config.downsample,
+    )
+
+    # take subset
+    seed = config.seed
+    n_files = config.n_files
+    if seed is not None:
+        np.random.seed(seed)
+        indices = np.random.choice(len(dataset), n_files, replace=False)
+    else:
+        indices = range(n_files)
+
+    subset = Subset(dataset, indices)
+
+    diffuser_dir = os.path.join(os.path.join(original_path, "dataset"), "diffuser_images")
+    lensed_dir = os.path.join(os.path.join(original_path, "dataset"), "ground_truth_lensed")
+    psf_path = os.path.join(psf_path, "psf.tiff")
     output_dir_path = None
 
     # create output directory
