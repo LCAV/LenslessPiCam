00991178ff95db4595e977fcd9c4019ca43d01b7
Eric Bezzam <ebezzam@gmail.com>
2024-02-27 14:28:58 +0100

    Add support to upload multimask dataset to Hugging Face. (#116)

--- scripts/data/upload_dataset_huggingface.py
+++ scripts/data/upload_dataset_huggingface.py
@@ -18,6 +18,7 @@ import hydra
 import time
 import os
 import glob
+import numpy as np
 from datasets import Dataset, DatasetDict, Image
 from huggingface_hub import upload_file
 from lensless.utils.dataset import natural_sort
@@ -34,11 +35,21 @@ def upload_dataset(config):
     repo_id = config.repo_id
     hf_token = config.hf_token
     n_files = config.n_files
+    multimask = config.multimask
     assert hf_token is not None, "Please provide a HuggingFace token."
+
+    # get masks
+    files_masks = []
+    n_masks = 0
+    if multimask:
+        files_masks = glob.glob(os.path.join(config.lensless.dir, "masks", "*.npy"))
+        files_masks = natural_sort(files_masks)
+        n_masks = len(files_masks)
 
     # get lensless files
     files_lensless = glob.glob(os.path.join(config.lensless.dir, "*" + config.lensless.ext))
     files_lensless = natural_sort(files_lensless)
+    print(f"Number of lensless files: {len(files_lensless)}")
     if n_files is not None:
         print(f"Only keeping {n_files} files...")
         files_lensless = files_lensless[:n_files]
@@ -61,7 +72,7 @@ def upload_dataset(config):
 
     # check for attribute
     df_attr = None
-    if config.lensed.celeba_attr is not None:
+    if "celeba_attr" in config.lensed.keys():
         # load attribute txt file with pandas
         import pandas as pd
 
@@ -74,6 +85,16 @@ def upload_dataset(config):
         df_attr = df_attr.astype(bool)
         # to dict
         df_attr = df_attr.to_dict(orient="list")
+    if multimask:
+        # add label according to mask
+        mask_labels = [np.arange(n_masks)]
+        mask_labels = np.tile(mask_labels, (len(common_files), 1))
+        mask_labels = mask_labels.flatten()
+        mask_labels = mask_labels[: len(common_files)]
+        if df_attr is not None:
+            df_attr["mask_label"] = mask_labels
+        else:
+            df_attr = {"mask_label": mask_labels}
 
     # step 1: create Dataset objects
     def create_dataset(lensless_files, lensed_files, df_attr=None):
@@ -91,16 +112,35 @@ def upload_dataset(config):
 
     # train-test split
     test_size = config.test_size
-    n_test = int(test_size * len(common_files))
-    if df_attr is not None:
+    if multimask:
+        n_mask_test = int(test_size * n_masks)
+        # get indices from df_attr
+        test_indices = np.where(df_attr["mask_label"] < n_mask_test)[0]
+        train_indices = np.where(df_attr["mask_label"] >= n_mask_test)[0]
         # split dict into train-test
-        df_attr_test = {k: v[:n_test] for k, v in df_attr.items()}
-        df_attr_train = {k: v[n_test:] for k, v in df_attr.items()}
+        test_dataset = create_dataset(
+            [lensless_files[i] for i in test_indices],
+            [lensed_files[i] for i in test_indices],
+            {k: [v[i] for i in test_indices] for k, v in df_attr.items()},
+        )
+        train_dataset = create_dataset(
+            [lensless_files[i] for i in train_indices],
+            [lensed_files[i] for i in train_indices],
+            {k: [v[i] for i in train_indices] for k, v in df_attr.items()},
+        )
     else:
-        df_attr_test = None
-        df_attr_train = None
-    test_dataset = create_dataset(lensless_files[:n_test], lensed_files[:n_test], df_attr_test)
-    train_dataset = create_dataset(lensless_files[n_test:], lensed_files[n_test:], df_attr_train)
+        n_test = int(test_size * len(common_files))
+        if df_attr is not None:
+            # split dict into train-test
+            df_attr_test = {k: v[:n_test] for k, v in df_attr.items()}
+            df_attr_train = {k: v[n_test:] for k, v in df_attr.items()}
+        else:
+            df_attr_test = None
+            df_attr_train = None
+        test_dataset = create_dataset(lensless_files[:n_test], lensed_files[:n_test], df_attr_test)
+        train_dataset = create_dataset(
+            lensless_files[n_test:], lensed_files[n_test:], df_attr_train
+        )
 
     # step 2: create DatasetDict
     dataset_dict = DatasetDict(
@@ -139,6 +179,15 @@ def upload_dataset(config):
         repo_type="dataset",
         token=hf_token,
     )
+
+    for _mask_file in files_masks:
+        upload_file(
+            path_or_fileobj=_mask_file,
+            path_in_repo=os.path.join("masks", os.path.basename(_mask_file)),
+            repo_id=repo_id,
+            repo_type="dataset",
+            token=hf_token,
+        )
 
     # total time in minutes
     print(f"Total time: {(time.time() - start_time) / 60} minutes")
