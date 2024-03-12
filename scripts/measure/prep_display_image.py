00f8c697e92209c71ae290164dd9509dc8a69a0a
Eric Bezzam <ebezzam@gmail.com>
2024-03-12 15:05:16 +0100

    Add support for multimask measurement. (#121)

--- scripts/measure/prep_display_image.py
+++ scripts/measure/prep_display_image.py
@@ -61,12 +61,29 @@ import click
     type=int,
     help="How many times to rotate provided image by 90 degrees.",
 )
-def display(fp, pad, output_path, vshift, brightness, screen_res, hshift, rot90):
+@click.option(
+    "--landscape",
+    is_flag=True,
+    help="Force landscape.",
+)
+@click.option(
+    "--image_res",
+    default=None,
+    nargs=2,
+    type=int,
+    help="Image resolution in pixels (width, height).",
+)
+def display(
+    fp, pad, output_path, vshift, brightness, screen_res, hshift, rot90, landscape, image_res
+):
     interpolation = cv2.INTER_NEAREST
 
     # load image
     img_og = cv2.imread(fp, cv2.IMREAD_UNCHANGED)
     img_og = cv2.cvtColor(img_og, cv2.COLOR_BGR2RGB)
+    if landscape:
+        if img_og.shape[0] > img_og.shape[1]:
+            img_og = np.rot90(img_og)
 
     # rotate image
     if rot90:
@@ -80,30 +97,31 @@ def display(fp, pad, output_path, vshift, brightness, screen_res, hshift, rot90)
         image_height, image_width = img_og.shape[:2]
         img = np.zeros((screen_res[1], screen_res[0], 3), dtype=img_og.dtype)
 
-        # set image with padding and correct aspect ratio
-        if screen_res[0] < screen_res[1]:
+        if image_res is None:
+            # set image with padding and correct aspect ratio
+            if screen_res[0] < screen_res[1]:
 
-            max_ratio = screen_res[1] / float(image_height)
+                max_ratio = screen_res[1] / float(image_height)
 
-            new_width = int(screen_res[0] / (1 + 2 * pad / 100))
-            ratio = new_width / float(image_width)
-            # new_height = int(ratio * image_height)
+                new_width = int(screen_res[0] / (1 + 2 * pad / 100))
+                ratio = new_width / float(image_width)
+                # new_height = int(ratio * image_height)
 
-        else:
+            else:
 
-            max_ratio = screen_res[0] / float(image_width)
+                max_ratio = screen_res[0] / float(image_width)
 
-            new_height = int(screen_res[1] / (1 + 2 * pad / 100))
-            ratio = new_height / float(image_height)
-            # new_width = int(ratio * image_width)
+                new_height = int(screen_res[1] / (1 + 2 * pad / 100))
+                ratio = new_height / float(image_height)
+                # new_width = int(ratio * image_width)
 
-        ratio = min(ratio, max_ratio)
-        new_width = int(ratio * image_width)
-        new_height = int(ratio * image_height)
+            ratio = min(ratio, max_ratio)
+            new_width = int(ratio * image_width)
+            new_height = int(ratio * image_height)
+            image_res = (new_width, new_height)
 
-        image_res = (new_width, new_height)
+        # set image within screen
         img_og = cv2.resize(img_og, image_res, interpolation=interpolation)
-
         img[: image_res[1], : image_res[0]] = img_og
 
         # center
