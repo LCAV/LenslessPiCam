377a34f23f3bd57e367489a121073c5fd4aef8af
mimanerhn <116755181+mimanerhn@users.noreply.github.com>
2025-05-26 20:41:38 +0200

    change

--- lenslesswebapp/server/index.cjs
+++ lenslesswebapp/server/index.cjs
@@ -36,7 +36,7 @@ app.post('/run-demo', async (req, res) => {
     await runCommand(`PYTHONPATH=.. python3 ../scripts/measure/on_device_capture.py \
       sensor=rpi_hq bayer=True fn=test_psf/raw_data.png \
       exp=1 iso=100 config_pause=2 sensor_mode=0 \
-      nbits_out=12 legacy=True rgb=False gray=False sixteen=True -- plot=True`);
+      nbits_out=12 legacy=True rgb=False gray=False sixteen=True`);
 
 
     // 2. Color correction
