bd14d48095314a7539acebd048ffa7ef3a28b079
mimanerhn <116755181+mimanerhn@users.noreply.github.com>
2025-05-26 16:08:40 +0200

    webapp

---
+++ lenslesswebapp/src/main.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import App from './App.tsx'
+import ProductPage from './ProductPage.tsx'
+import DemoPage from './DemoPage.tsx'
+
+import './index.css' 
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/product" element={<ProductPage />} />
+        <Route path="/demo" element={<DemoPage />} />
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>
+)
