ba01ee793a895fb825ae1867bbac25d02d4e0dd0
mimanerhn <116755181+mimanerhn@users.noreply.github.com>
2025-05-26 22:46:10 +0200

    yo

--- lenslesswebapp/vite.config.ts
+++ lenslesswebapp/vite.config.ts
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import fs from 'fs';
+import path from 'path';
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  server: {
+    https: {
+      key: fs.readFileSync(path.resolve(__dirname, 'certs/key.pem')),
+      cert: fs.readFileSync(path.resolve(__dirname, 'certs/cert.pem')),
+    },
+    host: true,
+    port: 5173,
+  },
+});
